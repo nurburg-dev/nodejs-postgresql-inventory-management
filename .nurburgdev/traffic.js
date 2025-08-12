c76e004156e0ae88034ac15a0a6ce1510d59f768
Anunay Biswas <anunay.biswas@gmail.com>
2025-08-12 12:52:07 +0000

    Update API tests

--- .nurburgdev/traffic.js
+++ .nurburgdev/traffic.js
@@ -91,7 +91,6 @@ export const options = {
   thresholds: {
     http_req_duration: ["p(95)<2000"],
     http_req_failed: ["rate<0.05"],
-    "http_req_duration{scenario:load_test}": ["p(90)<1000"],
   },
 };
 
